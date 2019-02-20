6868ed584f50ec22d033238619d8d7488dda6534
yongsheng1992 <mrzys@outlook.com>
2019-02-20 21:32:35 +0800

    feat: a simple echo server

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pyae
 
 pyae(python asynchronous event loop)，是一个模仿redis的异步事件编程库的python版本。只用来处理TCP网络编程。
+
+实现的EchoServer代码很凌乱，但是会不断的改进。
--- pyae/server.py
+++ pyae/server.py
@@ -4,7 +4,8 @@
     A simple TCP server implemented by pyae event loop.
 """
 import socket
-from .ae import EventLoop, AE_READABLE
+import errno
+from .ae import EventLoop, AE_READABLE, AE_WRITEABLE
 
 
 class Server:
@@ -15,18 +16,47 @@ class Server:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
 
-    def handle_read(self, ae: EventLoop, fd, mask):
-        pass
+    def handle_read(self, el: EventLoop, fd, mask):
+        fe = el.fes[fd]
+        client = fe.client
+        r_buffers = fe.r_buffers
 
-    def handle_write(self, ae: EventLoop, fd, mask):
-        pass
+        while True:
+            try:
+                buffers = client.recv(4096)
+                r_buffers += buffers
+            except socket.error as e:
+                err = e.args[0]
+                if err in (errno.EAGAIN, errno.EWOULDBLOCK):
+                    break
 
-    def handle_accept(self, ae: EventLoop, fd, mask):
-        conn, addr = self.sock.accept()
-        self.el.create_fe(conn.fileno(), fd, AE_READABLE)
+        before, seq, after = r_buffers.partition(b'\n')
+
+        if seq:
+            fe.w_buffers = before + seq
+            el.create_fe(fd, AE_WRITEABLE, self.handle_write, None)
+            fe.r_buffers = after
+
+    def handle_write(self, el: EventLoop, fd, mask):
+        fe = el.fes[fd]
+        client = fe.client
+        w_buffers = fe.w_buffers
+        client.sendall(w_buffers)
+        # todo: optimize it
+        el.delete_fe(fd, mask)
+
+    def handle_accept(self, el: EventLoop, fd, mask):
+        fe = el.fes[fd]
+        fe.mask = mask
+        client = fe.client
+
+        conn, addr = client.accept()
+        conn.setblocking(False)
+        el.create_fe(conn.fileno(), AE_READABLE, self.handle_read, conn)
 
     def run(self):
         self.sock.bind(self.address)
         self.sock.listen(8046)
         self.sock.setblocking(False)
-        self.el.create_fe(self.sock.fileno(), AE_READABLE, self.handle_accept)
+        self.el.create_fe(self.sock.fileno(), AE_READABLE, self.handle_accept, self.sock)
+        self.el.run()
